9b7c5a403220211f77df43421c57193fe2b9b95b
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-08-16 06:41:06 -0700

    task 3

--- sort.h
+++ sort.h
@@ -22,4 +22,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 size_t smallest(int *array, size_t size);
+void swap(int *a, int *b);
+int partition(int *arr, int first, int last, size_t size);
+void quickSort(int *arr, int first, int last, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
